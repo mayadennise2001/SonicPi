993cad6b20024fa76b45b386f4b2d904b5277f24
mayadennise2001 <maya_ramirez@davincischools.org>
2018-02-01 14:44:22 -0800

    Add files via upload

---
+++ sleepingbeauty.rb
@@ -0,0 +1,28 @@
+use_bpm 130
+
+live_loop :random do
+  use_synth :piano
+  play :f4
+  sleep 2
+  play :e4
+  sleep 2
+  play :f4
+  sleep 2
+  play :d4
+  sleep 1
+  play :e4
+  sleep 1
+  play :f4
+  sleep 1
+  play :d4
+  sleep 1
+  play :e4
+  sleep 2
+  play :g4
+  sleep 1
+  play :a4
+  sleep 2
+end
+
+
+
