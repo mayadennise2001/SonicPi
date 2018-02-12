fa5f878ee6d2968392abcbea67f294e5a4c27bcf
mayadennise2001 <maya_ramirez@davincischools.org>
2018-02-12 14:29:04 -0800

    Add files via upload

---
+++ disney.rb
@@ -0,0 +1,36 @@
+#Once Upon A Dream From Disney's Sleeping Beauty
+#Sheet Music from https://musescore.com/user/5693966/scores/2349456
+use_bpm 130
+x=1
+live_loop :random do
+  use_synth :piano
+  play :f4, sustain: x+1
+  sleep 2
+  play :e4, sustain: x
+  sleep 2
+  play :f4, sustain: x
+  sleep 2
+  play :d4, sustain: x
+  sleep 1
+  play :e4, sustain: x
+  sleep 1
+  play :f4, sustain: x
+  sleep 1
+  play :d4, sustain: x
+  sleep 1
+  play :e4, sustain: x
+  sleep 2
+  play :g4, sustain: x
+  sleep 1
+  play :a4, sustain: x
+  sleep 2
+  play :f4, sustain: x
+  sleep 1
+  play :g4, slide: 3
+  sleep 3
+  play :g4
+  sleep 1
+end
+
+
+
